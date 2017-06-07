78bd2966120afc9a908f31a712dcbc9fba062fde
Mitch Downey <mitchldowney@gmail.com>
2017-06-07 00:40:41 -0500

    Update navigation tabs

---
+++ Podverse/ClipTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ClipTableViewCell.swift
+//  Podverse
+//
+//  Created by Mitchell Downey on 6/7/17.
+//  Copyright © 2017 Podverse LLC. All rights reserved.
+//
+
+import UIKit
+
+class ClipTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var podcastImage: UIImageView!
+    @IBOutlet weak var podcastTitle: UILabel!
+    @IBOutlet weak var episodeTitle: UILabel!
+    @IBOutlet weak var time: UILabel!
+    @IBOutlet weak var clipTitle: UILabel!
+    
+}
---
+++ Podverse/ClipsTableViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ClipsTableViewController.swift
+//  Podverse
+//
+//  Created by Mitchell Downey on 6/6/17.
+//  Copyright © 2017 Podverse LLC. All rights reserved.
+//
+
+import UIKit
+
+class ClipsTableViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
