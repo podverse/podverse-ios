e30c3e4702e3d5b702a209638261f7af29bf0885
Mitch Downey <mitchldowney@gmail.com>
2017-10-17 21:53:26 -0500

    Move checkFor methods into Utilities file

--- Podverse/Constants.swift
+++ Podverse/Constants.swift
@@ -302,23 +302,3 @@ struct Strings {
     }
     
 }
-
-func checkForConnectivity() -> Bool {
-    
-    guard PVReachability.shared.hasInternetConnection() else {
-        return false
-    }
-    
-    return true
-    
-}
-
-func checkForResults(results: [Any]?) -> Bool {
-    
-    guard let results = results, results.count > 0 else {
-        return false
-    }
-    
-    return true
-    
-}
