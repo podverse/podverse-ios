ce7232840ab6af4d2428e826094d859ab0742ec1
Mitch Downey <mitchldowney@gmail.com>
2017-11-05 02:33:14 -0600

    Reorder Episode alert actions to have Downloaded as first option

--- Podverse/EpisodesTableViewController.swift
+++ Podverse/EpisodesTableViewController.swift
@@ -589,13 +589,13 @@ extension EpisodesTableViewController:FilterSelectionProtocol {
         
         let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
         
-        alert.addAction(UIAlertAction(title: EpisodesFilter.allEpisodes.text, style: .default, handler: { action in
-            self.filterTypeSelected = .allEpisodes
+        alert.addAction(UIAlertAction(title: EpisodesFilter.downloaded.text, style: .default, handler: { action in
+            self.filterTypeSelected = .downloaded
             self.reloadEpisodeData()
         }))
         
-        alert.addAction(UIAlertAction(title: EpisodesFilter.downloaded.text, style: .default, handler: { action in
-            self.filterTypeSelected = .downloaded
+        alert.addAction(UIAlertAction(title: EpisodesFilter.allEpisodes.text, style: .default, handler: { action in
+            self.filterTypeSelected = .allEpisodes
             self.reloadEpisodeData()
         }))
         
