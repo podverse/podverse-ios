f9e17903ac5859e6bd58dbc9a552d17849878898
Mitch Downey <mitchldowney@gmail.com>
2017-12-05 01:43:55 -0600

    Handle subscribing / unsubscribing from server

--- Podverse/PVSubscriber.swift
+++ Podverse/PVSubscriber.swift
@@ -14,6 +14,12 @@ class PVSubscriber {
     static func subscribeToPodcast(feedUrlString: String?) {
         
         if let feedUrlString = feedUrlString {
+            
+            // TODO: add error handling / connectivity error
+            updatePodcastOnServer(feedUrl: feedUrlString, shouldSubscribe: true) { wasSuccessful in
+                //
+            }
+            
             let feedParser = PVFeedParser(shouldOnlyGetMostRecentEpisode: false, shouldSubscribe: true)
             feedParser.parsePodcastFeed(feedUrlString: feedUrlString)
             
@@ -25,12 +31,65 @@ class PVSubscriber {
         
     }
     
+    static func unsubscribeFromPodcast(feedUrlString: String?) {
+        
+        if let feedUrlString = feedUrlString {
+            
+            // TODO: add error handling / connectivity error
+            updatePodcastOnServer(feedUrl: feedUrlString, shouldSubscribe: false) { wasSuccessful in
+                //
+            }
+            
+            PVDeleter.deletePodcast(feedUrl: feedUrlString)
+            
+        }
+        
+    }
+    
     static func checkIfSubscribed(feedUrlString: String?) -> Bool {
         if let feedUrlString = feedUrlString, let _ = Podcast.podcastForFeedUrl(feedUrlString: feedUrlString) {
             return true
         } else {
             return false
         }
+    }
+    
+    static func updatePodcastOnServer(feedUrl:String, shouldSubscribe:Bool, completion: @escaping (_ wasSuccessful:Bool?) -> Void) {
+        
+        let urlEnding = shouldSubscribe == true ? "subscribe" : "unsubscribe"
+        
+        if let url = URL(string: BASE_URL + "podcasts/" + urlEnding) {
+            var request = URLRequest(url: url, cachePolicy: NSURLRequest.CachePolicy.reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 60)
+            request.httpMethod = "POST"
+            
+            guard let idToken = UserDefaults.standard.string(forKey: "idToken") else {
+                return
+            }
+            
+            request.setValue(idToken, forHTTPHeaderField: "authorization")
+            
+            let postString = "podcastFeedURL=" + feedUrl
+            request.httpBody = postString.data(using: .utf8)
+            
+            let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                
+                guard error == nil else {
+                    DispatchQueue.main.async {
+                        completion(false)
+                    }
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    completion(true)
+                }
+                
+            }
+            
+            task.resume()
+            
+        }
+        
     }
     
 }
