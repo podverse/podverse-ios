92462665c5cdcc5e9eb7b417f88b1c6c11dbedd7
Mitch Downey <mitchldowney@gmail.com>
2018-08-11 19:48:21 -0500

    Fix kPlaylistLinkCopiedToast

--- Podverse/PlaylistDetailTableViewController.swift
+++ Podverse/PlaylistDetailTableViewController.swift
@@ -268,7 +268,7 @@ class PlaylistDetailTableViewController: PVViewController {
             
             activityVC.completionWithItemsHandler = { activityType, success, items, error in
                 if activityType == UIActivityType.copyToPasteboard {
-                    self.showToast(message: kPlaylistLinkCopied)
+                    self.showToast(message: kPlaylistLinkCopiedToast)
                 }
             }
             
